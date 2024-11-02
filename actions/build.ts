409f318b49c9fce9b3afe1ae85e787db2780973a
share121 <2854631158@qq.com>
2024-11-02 15:04:10 +0800

    f

---
+++ actions/build.ts
@@ -0,0 +1,29 @@
+import { minify } from "npm:html-minifier";
+
+await Deno.mkdir("dist").catch(async () => {
+  await Deno.remove("dist", { recursive: true });
+  await Deno.mkdir("dist");
+});
+
+Deno.copyFile("src/HYZhuanShuF.ttf", "dist/HYZhuanShuF.ttf");
+
+const raw = await Deno.readTextFile("src/index.html");
+Deno.writeTextFile(
+  "dist/index.html",
+  minify(raw, {
+    collapseBooleanAttributes: true,
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    keepClosingSlash: true,
+    minifyCSS: true,
+    minifyJS: true,
+    removeComments: true,
+    removeOptionalTags: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+  }),
+);
